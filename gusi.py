bb773df59d18c6512e914f232c54b69d68f5002f
earlmckay <79713229+earlmckay@users.noreply.github.com>
2022-12-25 17:03:44 +0100

    Add files via upload
    Renaming the radio stations

--- gusi.py
+++ gusi.py
@@ -1,6 +1,5 @@
 import time
 import os
-import threading
 from gpiozero import Button
 from gpiozero import RotaryEncoder
 from signal import pause
@@ -13,25 +12,19 @@ current_station = 0;
 vol = 40;
 btn_clk = RotaryEncoder(23, 27, bounce_time=float)
 btn_sw = Button(22)
-GODI = "http://YOUR-CUSTOM-STREAM"
-DWG_DE = "https://server23644.streamplus.de/stream.mp3"
-DWG_RU = "https://server32349.streamplus.de/stream.mp3"
-SW_DE = "http://www.segenswelle.de:8000/deutsch"
-SW_PD = "http://www.segenswelle.de:8000/plautdietsch"
-SW_RU = "http://www.segenswelle.de:8000/russisch"
+RS1 = "http://www.segenswelle.de:8000/deutsch"
+RS2 = "https://server23644.streamplus.de/stream.mp3"
+RS3 = "https://server32349.streamplus.de/stream.mp3"
 
 #---------- RADIO STATIONS ORDER ----------#
-stations = [GODI, DWG_DE, DWG_RU, SW_DE, SW_PD, SW_RU]
+stations = [RS1, RS2, RS3]
 
 #---------- ANNOUNCEMENTS ORDER ----------#
-announcements = ["godi.mp3", "dwg_de.mp3", "dwg_ru.mp3", "sw_de.mp3", "sw_pd.mp3", "sw_ru.mp3"]
+announcements = ["rs1.mp3", "rs2.mp3", "rs3.mp3"]
 
 #---------- DEFINITIONS ----------#
 def change_station():
     global current_station
-    global timer
-    if timer is not None:
-        timer.cancel()
     print("changing station from " + str(current_station))
     current_station = (current_station + 1) % len(stations)
     play(current_station)
@@ -41,34 +34,8 @@ def play(current_station):
     sudo_mpc("stop")
     sudo_mpc("clear")
     sudo_mpc("add " + announcements[current_station])
-    if current_station == 0:
-        sudo_mpc("add godi_offline.mp3")
-        connect_godi()
-    else:
-        sudo_mpc("add " + stations[current_station])
-        sudo_mpc("play")
-
-def connect_godi():
-    global timer
-    try:
-        response = urlopen("http://YOUR-CUSTOM-STREAM")
-    except:
-        print('Godi ist offline')
-        sudo_mpc("repeat on")
-        sudo_mpc("play")
-        #IN CASE OF TREAD PROBLEMS LOOK HERE#
-        timer = threading.Timer(60, connect_godi)
-        timer.start()
-    else:
-        print('Godi ist online')
-        sudo_mpc("clear")
-        sudo_mpc("repeat off")
-        sudo_mpc("add " + announcements[current_station])
-        sudo_mpc("add " + stations[current_station])
-        sudo_mpc("add godi_ended.mp3")
-        sudo_mpc("play")
-
-timer=None
+    sudo_mpc("add " + stations[current_station])
+    sudo_mpc("play")
 
 def check_connection():
 
@@ -123,7 +90,7 @@ def vol_down():
 
 #---------- START ----------#
 sudo_mpc("clear")
-sudo_mpc("volume 40")
+sudo_mpc("volume 35")
 sudo_mpc("repeat off")
 check_connection()
 play(current_station)
